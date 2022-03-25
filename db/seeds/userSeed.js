61930b037db6c2ebce2abc550ca90f70f5368646
aditore <anthonyditore@gmail.com>
2022-03-25 14:57:18 -0700

    adding seed data

---
+++ db/seeds/userSeed.js
@@ -0,0 +1,12 @@
+const userData = [
+    {
+        username: 'anthony',
+        password: 'anthony1'
+    },
+    {
+        username: 'maxwell',
+        password: 'maxwell1'
+    }
+];
+
+module.exports = userData;
